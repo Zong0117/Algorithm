6cd0c7d8cc2a9fd0e02d7db0ee679eb51ff933e7
Zong0117 <www.295102885@qq.com>
2021-07-26 17:16:21 +0800

    添加项目文件。

---
+++ 04 希尔排序/04 希尔排序.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+void swap(int* num1, int* num2)
+{
+    int* temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+void print(int arr[], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+void ShellSort(int* arr, int len)
+{
+    for (int gap = len / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < len; i++)
+        {
+            int key = arr[i];
+            int j = i - gap;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + gap] = arr[j];
+                j -= gap;
+            }
+            arr[j + gap] = key;
+        }  
+    }
+    print(arr, len);
+}
+int main()
+{
+    int arr[] = { 5,2,8,3,6,4,1,9,7,10 };
+    int len = sizeof(arr) / sizeof(arr[0]);
+    ShellSort(arr, len);
+}
---
+++ 2选择排序/2选择排序.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+void swap(int* num1, int* num2)
+{
+    int* temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+void print(int arr[], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+void SelectSort(int arr[], int len)
+{
+    int min;
+    for (int i = 0; i < len; i++)
+    {
+        min = i;
+        for (int j = i; j < len; j++)
+        {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        swap(arr[i], arr[min]);
+    }
+    print(arr, len);
+}
+int main()
+{
+    int arr[] = { 5,2,8,3,6,4,1,9,7,10 };
+    int len = sizeof(arr) / sizeof(arr[0]);
+    SelectSort(arr,len);
+}
+
